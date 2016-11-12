a5defad723414d48fb3ce85f1e1b8ff641998528
Jason Arnold <jason.arnold.00@gmail.com>
2016-11-12 16:54:48 -0500

    fix url issues

--- adapters/getAlbumArticlesAdapter.js
+++ adapters/getAlbumArticlesAdapter.js
@@ -1,3 +1,19 @@
-function getAlbumArticlesAdapter(){
-  alert('hi')
+function getAlbumArticlesAdapter(albumData){
+  event.preventDefault()
+  var artist_name = albumData.artist_name.split(" ").join("%20");
+  var album_name = albumData.name.split(" ").join("%20");
+  var url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=ea03314c542149f188e6b1f2d26bb3ea&q=${albumData.artist_name}%20${albumData.name}&fq=news_desk%3A%20Arts`;
+  // url += '?' + $.param({
+  //   'api-key': "ea03314c542149f188e6b1f2d26bb3ea",
+  //   'q': "",
+  //   'fq': "news_desk: Arts"
+  // });
+  $.ajax({
+    url: url,
+    method: 'GET'
+  }).done(function(data) {
+    console.log(data);
+  }).fail(function(err) {
+    throw err;
+  });
 }
