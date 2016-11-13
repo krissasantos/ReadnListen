0029a55d8565008809bd03495d004f255936541f
Jason Arnold <jason.arnold.00@gmail.com>
2016-11-13 18:03:46 -0500

    add empty div feature

--- adapters/getArtistsAdapter.js
+++ adapters/getArtistsAdapter.js
@@ -2,6 +2,7 @@ function getArtistsAdapter(){
   event.preventDefault();
   var artist_name = $('#artist_query').val();
 
+  $("#artist, #albums, #articles").empty()
   $.ajax({
     method: "GET",
     url: `https://api.spotify.com/v1/search?q=${artist_name}&type=artist`,
--- controllers/artistsController.js
+++ controllers/artistsController.js
@@ -2,15 +2,15 @@ function artistsController(data){
   var src = $("#artists-template")[0].innerHTML
   var template = Handlebars.compile(src)
   var artistList = template(data.artists.items)
-  $("#artist_name").append(artistList)
+  $("#artist").append(artistList)
 }
 
 function chooseArtist(artist_obj){
   const name = artist_obj.dataset.artist_name;
   const spotify_id = artist_obj.dataset.artist_id;
   const chosen_artist = new Artist(name, spotify_id);
-  $('#artist_name').empty()
-  $('#artist_name').append(`<br /><a href="#" data-name="${chosen_artist.name}" onclick="getArtistArticlesAdapter(this)">${chosen_artist.name}</a>`)
-  $('#artist_name').append(`<img src='${artist_obj.dataset.artist_pic}'/>`)
+  $('#artist').empty()
+  $('#artist').append(`<br /><a href="#" data-name="${chosen_artist.name}" onclick="getArtistArticlesAdapter(this)">${chosen_artist.name}</a>`)
+  $('#artist').append(`<img src='${artist_obj.dataset.artist_pic}'/>`)
   getAlbumsAdapter(chosen_artist);
 }
