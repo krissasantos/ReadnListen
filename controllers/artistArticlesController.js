1e71aca34a091e11cff25dbfc818b172824ca35a
krissasantos <krissa.santos31@gmail.com>
2016-11-14 16:02:55 -0500

    added simple format for list of articles

--- controllers/artistArticlesController.js
+++ controllers/artistArticlesController.js
@@ -1,8 +1,11 @@
 function artistArticlesController(data){
   // singler(data.items)
+
   $("#albums").empty();
+   
   var src = $("#artistArticles-template")[0].innerHTML
   var template = Handlebars.compile(src)
   var articleList = template(data.response.docs)
   $("#articles").append(articleList)
+
 }
