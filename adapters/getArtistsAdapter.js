3e74060ce8062d8033b0d1570f8d75e1d30a4782
Jason Arnold <jason.arnold.00@gmail.com>
2016-11-14 09:23:26 -0500

    add empty function when button is clicked

--- adapters/getArtistsAdapter.js
+++ adapters/getArtistsAdapter.js
@@ -2,7 +2,7 @@ function getArtistsAdapter(){
   event.preventDefault();
   var artist_name = $('#artist_query').val();
 
-  $("#artist, #albums, #articles, #errors").empty()
+  $("div").empty()
   $.ajax({
     method: "GET",
     url: `https://api.spotify.com/v1/search?q=${artist_name}&type=artist`,
