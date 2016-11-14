d31e38a3baecc314bd5b89ea22412e3cb622c85a
krissasantos <krissa.santos31@gmail.com>
2016-11-14 18:07:53 -0500

    add a deeper level of search query arts>music

--- adapters/getAlbumArticlesAdapter.js
+++ adapters/getAlbumArticlesAdapter.js
@@ -2,7 +2,7 @@ function getAlbumArticlesAdapter(albumData){
   event.preventDefault()
   var artist_name = albumData.artist_name.split(" ").join("%20");
   var album_name = albumData.name.split(" ").join("%20");
-  var url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=ea03314c542149f188e6b1f2d26bb3ea&q=${albumData.artist_name}%20${albumData.name}&fq=news_desk%3A%20Arts`;
+  var url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=ea03314c542149f188e6b1f2d26bb3ea&q=${albumData.artist_name}%20${albumData.name}&fq=news_desk%3A%20Arts%20Music`;
   $.ajax({
     url: url,
     method: 'GET'
