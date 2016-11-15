a0dc138d81901876deb01d488af7bdaf68a0f23a
Jason Arnold <jason.arnold.00@gmail.com>
2016-11-15 09:51:58 -0500

    changed value of mini nav placeholder and button

--- adapters/getArtistsAdapter.js
+++ adapters/getArtistsAdapter.js
@@ -2,7 +2,7 @@ function getArtistsAdapter(){
   event.preventDefault();
   var artist_name = $('#artist_query').val();
   $("div").not(".container-fluid").empty()
-  $("#nav").append('<form><input type="text" name="artist_query" placeholder="Artist Name" id="artist_query"/><input type="submit" class="btn btn-default" onclick="return validateForm()"/></form>');
+  $("#nav").append('<form><input type="text" name="artist_query" placeholder="Choose Another Artist" id="artist_query"/><input type="submit" class="btn btn-default" onclick="return validateForm()" value="Go" /></form>');
   $.ajax({
     method: "GET",
     url: `https://api.spotify.com/v1/search?q=${artist_name}&type=artist`,
